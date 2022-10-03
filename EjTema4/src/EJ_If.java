07a0f907063234bfa010e375ef0f63bb7571d806
nicolas899 <nicolasgaitan899@gmail.com>
2022-10-02 22:34:27 -0300

    nuevo ejercicio

---
+++ EjTema4/src/EJ_If.java
@@ -0,0 +1,19 @@
+public class EJ_If {
+
+    public void ejercicio1(){
+
+    System.out.println("Positivo o Negativo");
+        System.out.println();
+
+    int valor = 5;
+
+        if (valor > 0){
+        System.out.println("Es un numero positivo");
+    } else if (valor < 0) {
+        System.out.println("El valor es Negativo");
+    }else {
+        System.out.println("El valor es 0");
+    }
+
+}
+}
---
+++ EjTema4/src/Ej_For.java
@@ -0,0 +1,11 @@
+public class Ej_For {
+
+    public void Ejercicio4(){
+
+        for (int v = 0; v <= 3; v++){
+            System.out.println(v);
+
+        }
+
+    }
+}
