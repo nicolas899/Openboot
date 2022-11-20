3951a622db588ffe3c3afa63635e3dc26d127092
nicolas899 <nicolasgaitan899@gmail.com>
2022-11-20 15:34:38 -0300

    base de datos

---
+++ PYTHON/clase11/conexion.py
@@ -0,0 +1,23 @@
+import mysql
+import mysql.connector
+
+cone = mysql.connector.connect(user='root', host='Localhost',database='openb',port='3306')
+
+cursor = cone.cursor()
+
+cursor.execute("CREATE TABLE Alumnos(Id INT, Nombre TEXT, Apellido TEXT)")
+
+cursor.execute("INSERT INTO Alumnos VALUES(1,'Nicolas', 'Gaitan')")
+cursor.execute("INSERT INTO Alumnos VALUES(2,'Angel', 'Di maria')")
+cursor.execute("INSERT INTO Alumnos VALUES(3,'Diego', 'Milito')")
+cursor.execute("INSERT INTO Alumnos VALUES(4,'Leo', 'Messi')")
+cursor.execute("INSERT INTO Alumnos VALUES(5,'Cristiano', 'Ronaldo')")
+cursor.execute("INSERT INTO Alumnos VALUES(6,'Roman', 'Riquelme')")
+cursor.execute("INSERT INTO Alumnos VALUES(7,'Pablo', 'Vegetti')")
+cursor.execute("INSERT INTO Alumnos VALUES(8,'Javier', 'Zanetti')")
+
+cone.commit()
+
+cone.close()
+
+print(cone)
